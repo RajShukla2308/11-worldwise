76385f3d5ec63af1e41649710b6e713fbe5a28db
Raj Shukla <rajshukla9630@gmail.com>
2026-05-24 15:29:43 +0530

    L-211 building main app lyout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/SideBar";
+import styles from './AppLayout.module.css';
+import Map from "../components/Map";
+
 
 export default function AppLayout(){
   return (
-    <div>
-        <AppNav />
-
-        App Layout
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   )
 }
