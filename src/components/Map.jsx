f8ca2b2e978b839199404951507c63cee145178f
Raj Shukla <rajshukla9630@gmail.com>
2026-05-24 23:14:54 +0530

    L-218 programatic navigation with useNavigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,16 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 export default function Map(){
 
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const navigate = useNavigate();
+
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
-  console.log(lat,lng);
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={()=> navigate('form')}>
       <h1>{lat}</h1> 
       <h1>{lng}</h1>
     </div>
