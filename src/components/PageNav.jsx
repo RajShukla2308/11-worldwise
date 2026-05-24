e202ea3ee1fc32406b90240d9f3fd103ff7dce60
Raj Shukla <rajshukla9630@gmail.com>
2026-05-24 14:25:54 +0530

    L-209 module css and adding starter folder

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,20 @@
-import {  NavLink } from "react-router-dom"
+import {  NavLink } from "react-router-dom";
+import styles from './PageNav.module.css';
 
 export default function PageNav(){
   return (
-    <ul>
-        <li>
-            <NavLink to="/">Home Page</NavLink>
-        </li>
-        <li>
-            <NavLink to="/pricing">Pricing</NavLink>
-        </li>
-        <li>
-            <NavLink to="/product">Product</NavLink>
-        </li>
-    </ul>
+    <nav className={styles.nav}>
+        <ul>
+            <li>
+                <NavLink to="/">Home Page</NavLink>
+            </li>
+            <li>
+                <NavLink to="/pricing">Pricing</NavLink>
+            </li>
+            <li>
+                <NavLink to="/product">Product</NavLink>
+            </li>
+        </ul>
+    </nav>
   )
 }
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import AppNav from "../components/AppNav";
+
+export default function AppLayout(){
+  return (
+    <div>
+        <AppNav />
+
+        App Layout
+    </div>
+  )
+}
