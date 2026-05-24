5d0b46e5ea2cf2436f192fa4325c94a9bb15c49b
Raj Shukla <rajshukla9630@gmail.com>
2026-05-24 23:21:45 +0530

    L-219 programatic navigation with Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes , Route} from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -46,8 +47,7 @@ export default function App(){
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} >
-          <Route index element={<CityList cities={cities} 
-          isLoading={isLoading} />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route path="cities" element={<CityList cities={cities} 
           isLoading={isLoading}/>} />
           <Route path="cities/:id" element={<City />} />
