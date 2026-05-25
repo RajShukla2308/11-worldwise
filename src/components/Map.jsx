23382c1b5253fb8b5476cd6736121268300e69e1
Raj Shukla <rajshukla9630@gmail.com>
2026-05-25 18:29:16 +0530

    L-230-232 map functionalities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,76 @@
+/* eslint-disable react/prop-types */
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { useCities } from '../contexts/CityContext';
+
+
+
+
 export default function Map(){
 
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
+   // eslint-disable-next-line no-unused-vars
+  const mapLat = searchParams.get('lat') || 40; const mapLng = searchParams.get('lng') || 0;
 
-  const navigate = useNavigate();
 
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  // eslint-disable-next-line no-unused-vars
+  const [mapPosition, setMapPosition] = useState([40,0])
+
+  const {cities} = useCities();
+
+
+  useEffect(function(){
+    if(mapLat && mapLng) setMapPosition([mapLat,mapLng])
+  },[mapLat,mapLng])
+
+  
+
+
   return (
-    <div className={styles.mapContainer} onClick={()=> navigate('form')}>
-      <h1>{lat}</h1> 
-      <h1>{lng}</h1>
+    <div className={styles.mapContainer}>
+
+    <MapContainer
+    center={mapPosition} 
+      // center={[mapLat,mapLng]}
+     className={styles.map} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {cities.map(city=>{
+        return <>
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+        <Popup>
+          <span>{city.emoji} </span>
+           <span>{city.cityName} </span>
+        </Popup>
+      </Marker></>
+      }
+       
+      )
+      }
+      <ChangeCenter position={mapPosition} />
+      <DetectClick />
+  </MapContainer>
+      
     </div>
   )
+}
+
+function ChangeCenter({position}){
+  const map = useMap();
+  map.setView(position)
+  return null;
+}
+
+function DetectClick(){
+  const navigate = useNavigate();
+
+  useMapEvents({
+    // eslint-disable-next-line no-unused-vars
+    click: (e)=> navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+  })
 }
